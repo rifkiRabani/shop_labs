6a7559e0695308958473290507e50228143a65b4
Rifki Ahmad Rabani <rifki.rabani@ist.id>
2026-09-22 14:12:05 +0700

    adjusted assertion

--- test_cases/login.py
+++ test_cases/login.py
@@ -6,6 +6,7 @@ from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from conftest import BASE_URL, allure_metadata
 
+
 @allure_metadata(
     title="Valid login with valid credentials.",
     description="this test case will validate user login with happy flow",
@@ -19,6 +20,12 @@ def test_valid_login(chrome):
     login_page = LoginPage(chrome)
     dashboard_page = DashboardPage(chrome)
     chrome.goto(BASE_URL)
-    
-    
-    
+    login_page.login("demo@demo.com", "demo")
+    dashboard_page.should_be_open()
+
+# def test_invalid_username(chrome):
+#     login_page = LoginPage(chrome)
+#     dashboard_page = DashboardPage(chrome)
+#     chrome.goto(BASE_URL)
+#     assert login_page.should_be_open()
+#     login_page.login("invalid_username", "valid_password")
